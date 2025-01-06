761d2d58160bd12cc35c68274f4a7b51d103691c
Arafet Ben Kilani <arafet.benkilani@supcom.tn>
2025-01-06 16:32:55 +0100

    unexport Logger field

--- internal/logging/slog.go
+++ internal/logging/slog.go
@@ -8,27 +8,27 @@ import (
 )
 
 type slogLogger struct {
-	Logger *slog.Logger
+	logger *slog.Logger
 }
 
 func NewSlogLogger(writer io.Writer, level slog.Level) *slogLogger {
 	return &slogLogger{
-		Logger: slog.New(tint.NewHandler(writer, &tint.Options{Level: level})),
+		logger: slog.New(tint.NewHandler(writer, &tint.Options{Level: level})),
 	}
 }
 
 func (s *slogLogger) Debug(msg string, args ...any) {
-	s.Logger.Debug(msg, args...)
+	s.logger.Debug(msg, args...)
 }
 
 func (s *slogLogger) Info(msg string, args ...any) {
-	s.Logger.Info(msg, args...)
+	s.logger.Info(msg, args...)
 }
 
 func (s *slogLogger) Warn(msg string, args ...any) {
-	s.Logger.Warn(msg, args...)
+	s.logger.Warn(msg, args...)
 }
 
 func (s *slogLogger) Error(msg string, args ...any) {
-	s.Logger.Error(msg, args...)
+	s.logger.Error(msg, args...)
 }
